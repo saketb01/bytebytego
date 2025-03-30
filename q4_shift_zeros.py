9c8bd518c03b63cfce205a9cd75c054daf66f9c9
saketb01 <saketbarve@gmail.com>
2025-03-30 17:40:01 -0400

    Sync changes

--- q4_shift_zeros.py
+++ q4_shift_zeros.py
@@ -1,19 +1,24 @@
 from typing import List
-"""
+
+'''
 Q4: Given an array of integers, modify the array in place to move all zeros 
 to the end while maintaining the relative order of non-zero elements.
-"""
+'''
+
+
 def shift_zeros_to_the_end(nums: List[int]) -> None:
+
     if not nums or len(nums) == 1:
         return
     
+    last_zeroth_index = 0
     nums_length = len(nums)
     first_index = 0
     second_index = 1
     while(second_index < nums_length):
         if(nums[second_index] == 0 and nums[first_index] != 0):
             # Swap and increment 
-            nums[second_index] = nums[first_index]
+            nums[second_index], nums[first_index] = nums[first_index], nums[second_index]
             first_index += 1
             second_index += 1
         elif(nums[first_index]== 0 and nums[second_index] == 0):
@@ -28,10 +33,12 @@ def shift_zeros_to_the_end(nums: List[int]) -> None:
             # Increment both indices
             first_index += 1
             second_index += 1   
+        print(" ***** nums: " + str(nums) + " first_index: " + str(first_index) + " second_index: " + str(second_index))
+        print("\n\n")
 
     print(nums)
 
 if __name__ == "__main__":
-    nums = [0, 1, 0, 3, 2]
+    nums = [1, 0, 1, 0]
     shift_zeros_to_the_end(nums)
     print("All done")
